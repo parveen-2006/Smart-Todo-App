5a65b7b41c38a84f84660cf39a85438bb171c1ce
parveen-2006 <pavanbaghel2006@gmail.com>
2026-04-10 11:08:16 +0530

    setup

---
+++ Backend/routes/task.routes.js
@@ -0,0 +1,11 @@
+const expresss = require("express")
+const router = expresss.Router();
+
+router.get("/" , (req,res)=>{
+    res.status(200).json({
+        success : true, 
+        message : "Success"
+    })
+})
+
+module.exports = router;
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>We're about to make Smart todo app using backend</div>
+  )
+};
