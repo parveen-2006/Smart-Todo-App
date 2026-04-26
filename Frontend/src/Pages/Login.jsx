552adc33fe754376d88cc91a3faa94b208a6326a
parveen-2006 <pavanbaghel2006@gmail.com>
2026-04-26 15:09:47 +0530

    login working , connected to backend

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,17 +1,51 @@
 import React, { useState } from 'react'
+import instance from '../Services/Api';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Login() {
+
+    const navigate = useNavigate()
     const [login, setLogin] = useState({
-        name: "",
+        email: "",
         password: ""
     })
+    const handleChange = (e)  =>{
+        const {name , value} = e.target;
+        setLogin((prev)=>({...prev , [name] : value}));
+    } 
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try {
+            const response = await instance.post("/auth/login" , login)
+            localStorage.setItem("token" , response.data.token);
+            console.log(response.data);
+
+            if(response.data.success || response.data.token){
+                alert("login successful");
+                navigate("/")
+            }
+            
+        } catch (err) {
+            console.log("Login err :" ,err  );
+        }
+    }
     return (
         <div>
-            <form>
-                <input type="text"
+            <form onSubmit={handleSubmit}>
+                <input type="email"
                     placeholder='Enter your name'
-                    value={login.name}
-                    name="name" />
+                    value={login.email}
+                    name="email" 
+                    onChange={handleChange}
+                    />
+                <input type="password"
+                    placeholder='Enter your name'
+                    value={login.password}
+                    name="password" 
+                    onChange={handleChange}
+                    />
+                    <button type="submit">login</button>
             </form>
         </div>
     )
