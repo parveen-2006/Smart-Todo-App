e2de215838b532b309f219224d83b4da9b8c8a58
parveen-2006 <pavanbaghel2006@gmail.com>
2026-04-29 21:52:15 +0530

    verify token through middleware

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      return res.status(401).json({
+        message: "no token , not authorized",
+      });
+    }
+    const SECRET_KEY = "SMARTTODO";
+    const token = authHeader.spilt(" ")[1];
+
+    const decoded = jwt.verify(token, SECRET_KEY);
+
+    req.user = decoded.id;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "Token invalid or expired",
+    });
+  }
+};
+
+module.exports = protect;
